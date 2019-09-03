2888bf5d523afece3a872ac9449caf40e222e50c
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-09-03 16:35:01 -0300

    Update F.c

--- 2006/F.c
+++ 2006/F.c
@@ -5,19 +5,22 @@ int main(){
     int numeroParticipantes, partidasJogadas, pontuacao, cont=0;
     char times[20];
     
-    
-    scanf("%i", &numeroParticipantes);
-    scanf("%i", &partidasJogadas);
-    
-    for(int i=0; i<numeroParticipantes; i++){
-      scanf("%s", &times);
-      scanf("%i", &pontuacao);
-      printf("pontuacao == %i", pontuacao);
-      if(pontuacao == 1){
-	        cont++;
-	    }
+    while(1){
+      scanf("%i", &numeroParticipantes);
+      scanf("%i", &partidasJogadas);
+      if(numeroParticipantes == 0 && partidasJogadas == 0){
+        break;
+      }
+      
+      for(int i=0; i<numeroParticipantes; i++){
+        scanf("%s", &times);
+        scanf("%i", &pontuacao);
+        if(pontuacao == 1){
+            cont++;
+        }
+      }
+      printf("%i", cont);
     }
-    printf("%i", cont);
-    
+
     return 0;
 }
