1512fc0a5ce95eba2d5b65b5c98ca76fe19e7dfa
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-09-03 19:07:45 -0300

    Update D.c

--- 2008/D.c
+++ 2008/D.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+
 int main(){
-    bool a = true;
-    if(a){
-        printf("aaaaaa");
-    }
-    return 0;
+	int x1,x2,y1,y2, movimentos = 2;
+	while(1){
+		movimentos = 2;
+		scanf("%i", &x1);
+		scanf("%i", &y1);
+		scanf("%i", &x2);
+		scanf("%i", &y2);
+		
+		if(x1 == 0 && x2 == 0 && y1 == 0 && y2 == 0){
+			break;
+		}
+		
+		bool vertical = (x1 == x2 && y1 != y2);
+		bool horizontal = (x1 != x2 && y1 == y2);
+		bool diagonal = (abs(x1-x2) == abs(y1-y2));
+		
+		if(x1 == x2 && y1 == y2){
+			movimentos = 0;
+		}else if(vertical || horizontal || diagonal){
+			movimentos = 1;
+		}
+		printf("%i\n", movimentos);
+	}
+	return 0;
 }
