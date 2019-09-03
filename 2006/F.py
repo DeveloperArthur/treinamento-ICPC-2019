b23d26fa29b4e89d54722b16fd87e6267ba276bc
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-09-03 16:45:22 -0300

    Update F.py

--- 2006/F.py
+++ 2006/F.py
@@ -1,11 +1,16 @@
-cont=0
+while True:
+    cont=0
 
-numeroParticipantes = input()
-partidasJogadas = input()
+    numeroParticipantes = input()
+    partidasJogadas = input()
+    
+    if int(numeroParticipantes) == 0 and int(partidasJogadas) == 0:
+        break
+    
+    for i in range(0, int(numeroParticipantes)):
+        time = input()
+        pontuacao = input()
+        if int(pontuacao) == 1:
+            cont = cont+1
+    print(cont)
 
-for i in range(0, int(numeroParticipantes)):
-    time = input()
-    pontuacao = input()
-    if int(pontuacao) == 1:
-        cont = cont+1
-print(cont)
