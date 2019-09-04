2181ba1c640af0b80c39c897793cd77ed38f5817
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-09-04 16:19:02 -0300

    Create F.java

---
+++ 2011/F.java
@@ -0,0 +1,21 @@
+package a2011;
+
+import java.util.Scanner;
+
+public class F {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			int fotos = 0;
+			int n = sc.nextInt();
+			for(int i = 0; i<n; i++) {
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+				//CALCULO
+			}
+			System.out.println(fotos);
+		}
+	}
+}
