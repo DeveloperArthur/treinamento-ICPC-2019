213387f32e51ff3e9cd0328ecca18c55bf60e73c
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-09-05 15:05:11 -0300

    Create A.c

---
+++ 2012/A.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (){
+	int l, c, n, x;
+	int linhas, paginas, letras;
+	char palavra[71];
+
+	while(1){
+	    scanf("%d %d %d", &n, &l, &c);
+		paginas = 1;
+		linhas = 1;
+		scanf("%s", palavra);
+		letras = strlen(palavra);
+		n--;
+		for (int i = 0; i < n; ++i){
+            scanf("%s", palavra);
+            x = strlen(palavra);
+            if (c >= (letras + x + 1)){
+                letras += x + 1;
+            }else{
+                ++linhas;
+				if (linhas > l){
+				    ++paginas, linhas = 1;
+				}
+				letras = x;
+			}
+		}
+        printf("%d\n", paginas);
+    }
+    return 0;
+}
