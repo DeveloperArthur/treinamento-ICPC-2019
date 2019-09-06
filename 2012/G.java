829306c13d877d562f4009f18b4092b0f4be4453
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-09-06 13:35:50 -0300

    Update G.java

--- 2012/G.java
+++ 2012/G.java
@@ -30,18 +30,33 @@ public class G {
 				if(largada[k] != chegada[k]) {
 					//SE O NUMERO FOR DIFERENTE, PROCURA NO ARRAY DE LARGADA O GRID DE CHEGADA CORRESPONDENTE AO CHEGADA PRA TROCAR
 					for(int l=0; l<n; l++) {
-						//QUANDO ACHAR O NUMERO TROCA
 						if(largada[l] == chegada[k]) {
-							int aux = largada[k];
+							int valor_K = largada[k];
+							//System.out.println("VALOR_K == "+valor_K);							
 							largada[k] = largada[l];
-							largada[l] = aux;
-							
-							System.out.println("POSICAO LARGADA[l] == "+largada[l]);
-							System.out.println("POSICAO CHEGADA[] == "+k);
-							System.out.println("L == "+l);
-							System.out.println("K == "+k);
-							System.out.println();
+							//System.out.println("POSICAO "+k+" RECEBEU O VALOR DA POSICAO "+l);
+							//l = l-1
+							for(int o=l; o!=k; o--){
+								if(o==1) {
+									break;
+								}
+								//System.out.println("POSICAO "+o+" TROCANDO COM POSICAO "+(o-1));
+								int aux = largada[o];
+								largada[o] = largada[o-1];
+								largada[o-1] = aux;
+							}
+							largada[k+1] = valor_K;
+							//System.out.println("POSICAO "+l+" RECEBEU O VALOR_K == "+valor_K);
+							//System.out.println("POSICAO LARGADA[L] == "+l);
+							//System.out.println("POSICAO LARGADA[K] == "+k);
+							//System.out.println();
 							ultrapassagens += Math.abs(l-k);
+							//System.out.println("ULTRAPASSAGENS ATUAIS == "+ultrapassagens);
+							//System.out.println("LOG ATUAL:");
+							/*for(int m=0; m<n; m++) {
+								System.out.print(largada[m]+" ");
+							}
+							System.out.println();*/
 						}
 					}
 				}
