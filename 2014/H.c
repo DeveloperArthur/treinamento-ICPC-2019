1897eeed8c07bfcfa8744c799e3d06f9fc6f91f5
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-09-07 10:56:35 -0300

    Update H.c

--- 2014/H.c
+++ 2014/H.c
@@ -4,20 +4,20 @@ int main() {
 	int jogadores, partidas, gols=0;
 	int desempenho;
 	
-	while(scanf("%i %i", &jogadores, &partidas)){
-		for(int i=0; i<jogadores; i++){
-			int aux = 0;
-			for(int j=0; j<partidas; j++){
-				scanf("%i", &desempenho);
-				if(desempenho > 0){
-					aux++;
-				}
-			}
-			if(aux == partidas){
-				gols++;
+	scanf("%i %i", &jogadores, &partidas);
+	
+	for(int i=0; i<jogadores; i++){
+		int aux = 0;
+		for(int j=0; j<partidas; j++){
+			scanf("%i", &desempenho);
+			if(desempenho > 0){
+				aux++;
 			}
 		}
-		printf("%i", gols);
+		if(aux == partidas){
+			gols++;
+		}
 	}
+	printf("%i\n", gols);
 	return 0;
 }
