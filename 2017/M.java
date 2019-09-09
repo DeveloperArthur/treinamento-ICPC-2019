bca8730a86d62a2583113481ba8a4b81626bb477
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-09-09 16:49:52 -0300

    Create M.java

---
+++ 2017/M.java
@@ -0,0 +1,35 @@
+package a2017;
+
+import java.util.Scanner;
+
+public class M {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A1 = sc.nextInt();
+		int A2 = sc.nextInt();
+		int A3 = sc.nextInt();
+		
+		int total=0;
+		
+		if(A1 > A2 && A1 > A3) {
+			total = (A2*2)+(A3*4);
+			
+		}else if(A2 > A1 && A2 > A3) {
+			total = (A1*2)+(A3*2);
+			
+		}else if(A3 > A1 && A3 > A2) {
+			total = (A1*4)+(A2*2);
+			
+		}else if(A1 == A2 || A3 == A2) {
+			total = (A1*2)+(A3*2);
+			
+		}else if(A1 == A3) {
+			total = (A2*2)+(A3*4);
+			
+		}
+		
+		System.out.println(total);
+	}
+
+}
